978274eafb98bcc52c0e0e665afe64edae4ce8b3
PeroHub <65111015+PeroHub@users.noreply.github.com>
2022-11-19 12:45:48 -0800

    my va task

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import ErrorBoundary from './layout/error-boundary/ErrorBoundary';
+import GeneralLoading from './layout/general-loading/GeneralLoading';
+import Layout from './layout/Layout';
+import {
+  About,
+  Account,
+  Career,
+  Contact,
+  FAQ,
+  LandingPage,
+  Premium,
+  PrivatePolicy,
+  Task,
+  Error,
+} from './pages';
+
+function App() {
+  return (
+    <ErrorBoundary>
+      <Layout>
+        <Suspense fallback={<GeneralLoading text={`LOADING...`} />}>
+          <Routes>
+            <Route path='/' element={<LandingPage />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/career' element={<Career />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/policy' element={<PrivatePolicy />} />
+
+            <Route path='/faq' element={<FAQ />} />
+
+            {/* 404-error handler */}
+            <Route
+              path='*'
+              element={<Error />}
+            />
+          </Routes>
+        </Suspense>
+      </Layout>
+    </ErrorBoundary>
+  );
+}
+
+export default App;
---
+++ src/pages/Career/Career.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { CareerHero, CareerMiddle, JobOpening } from '../../Components/career';
+
+const Career = () => {
+  return (
+    <div>
+      <CareerHero />
+      <CareerMiddle />
+      <JobOpening />
+    </div>
+  );
+};
+
+export default Career;
